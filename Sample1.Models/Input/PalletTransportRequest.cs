6bfaeb9f3361ccf26930edf077d42d508c6527e0
Sean Alisea <33284012+sean-alisea@users.noreply.github.com>
2021-12-29 12:57:15 -0500

    Add files via upload

---
+++ Sample1.Models/Input/PalletTransportRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sample1.Models.Input
+{
+    public class PalletTransportRequest
+    {
+        public long loadId { get; set; }
+        public decimal x { get; set; }
+        public decimal y { get; set; }
+    }
+}
