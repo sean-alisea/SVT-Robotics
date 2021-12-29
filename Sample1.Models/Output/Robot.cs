60d0cf105f60ade2aadda0e033fc5b903b7b07db
Sean Alisea <33284012+sean-alisea@users.noreply.github.com>
2021-12-29 12:58:24 -0500

    Add files via upload

---
+++ Sample1.Models/Output/Robot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1.Models.Output
+{
+    public class Robot
+    {
+        public long robotId { get; set; }
+        public decimal distanceToGoal { get; set; }
+        public decimal batteryLevel { get; set; }
+    }
+}
