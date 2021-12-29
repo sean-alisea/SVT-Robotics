5c27f281cfd0be93797cc972b2de089825f40cc0
Sean Alisea <33284012+sean-alisea@users.noreply.github.com>
2021-12-29 12:55:34 -0500

    Add files via upload

---
+++ Sample1.Repository/IRobotRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sample1.Models.Input;
+using Sample1.Models.Output;
+
+namespace Sample1.Repository
+{
+    public interface IRobotRepository
+    {
+        public Task<Robot> CalculatePath(PalletTransportRequest s);
+    }
+}
---
+++ Sample1.Repository/RobotRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sample1.Models.Input;
+using Sample1.Models.Output;
+
+namespace Sample1.Repository
+{
+    public class RobotRepository : IRobotRepository
+    {       
+        async Task<Robot> IRobotRepository.CalculatePath(PalletTransportRequest s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
