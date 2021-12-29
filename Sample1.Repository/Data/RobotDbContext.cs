0d5f00f7419127130bba84aabadffdb34423fb56
Sean Alisea <33284012+sean-alisea@users.noreply.github.com>
2021-12-29 12:56:21 -0500

    Add files via upload

---
+++ Sample1.Repository/Data/RobotDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample1.Models.Output;
+
+namespace Sample1.Repository.Data
+{
+    public class RobotDbContext : DbContext
+    {
+        public DbSet<Robot> Categories { get; set; }
+    }
+}
