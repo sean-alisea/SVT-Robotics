201692b6853f6fc5ff4213de6e3f3169a7641e31
Sean Alisea <33284012+sean-alisea@users.noreply.github.com>
2021-12-29 13:09:20 -0500

    Add files via upload

---
+++ Sample1/Controllers/RobotsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Sample1.Models.Input;
+using Sample1.Models.Output;
+using Sample1.Repository;
+
+namespace Sample1.Controllers
+{
+    [ApiController]
+    [Route("api/robots")]
+    public class RobotsController : ControllerBase
+    {
+        private readonly ILogger<RobotsController> _logger;
+        private readonly IRobotRepository _repository;
+
+        public RobotsController(ILogger<RobotsController> logger, IRobotRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Closest(PalletTransportRequest l)
+        {             
+            try
+            {
+                Robot path = await this._repository.CalculatePath(l);
+
+                return CreatedAtAction("Closest", path);
+            }
+            catch (Exception e)
+            {
+                this._logger.LogError(e.Message);
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, "Error on POST.");
+            }
+        }
+    }
+}
